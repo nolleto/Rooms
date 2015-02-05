b1622852f9561959037243214667b29d2a2c4afd
Bruno Konrad <bruno.skon@gmail.com>
2015-02-05 12:53:23 -0200

    Primeiros arquivos, muito baseado no tutorial do @osuissa, valeu

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+var app 	= require('express')()
+	, http 	= require('http').Server(app)
+	, io 		= require('socket.io')(http)
+	;
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+
+	console.log('Conectou!');
+	socket.on('disconnect', function() {
+		console.log(allSocket[socket] + ' saiu... :(');
+	});
+
+	socket.on('chat message', function(msg) {
+
+	});
+
+});
+
+http.listen('3000', function() {
+	console.log('Começou o jogo, em localhost:3000');
+})
