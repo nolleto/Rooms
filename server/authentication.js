e5e58f409714ad300b03f6f58d66eb36bd0f119d
Bruno Konrad <bruno.skon@gmail.com>
2015-02-09 16:13:20 -0200

    Tentando remover os tabs... Vamo lá

--- server/authentication.js
+++ server/authentication.js
@@ -4,14 +4,13 @@ var user = require('./models/User');
 
 module.exports = {
   facebook: function(accessToken, refreshToken, profile, done) {
-		// Aqui tem que rolar a autenticação e taus
-		user.getUser(profile.id, function(docs) {
-			if (docs.length == 0) {
-				var name = profile.name.givenName + ' ' + profile.name.familyName;
-				user.insertUser(profile.id, name, function(result) {
-					console.log(result);
-				});
-			}
-		});
-	}
+    user.getUser(profile.id, function(docs) {
+      if (docs.length == 0) {
+        var name = profile.name.givenName + ' ' + profile.name.familyName;
+        user.insert(profile.id, name, function(result) {
+            console.log(result);
+        });
+      }
+    });
+  }
 };
