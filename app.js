fbe404ca15ed1e51f0615ff08d7011cf981520db
Bruno Konrad <bruno.skon@gmail.com>
2015-02-12 09:14:43 -0200

    Removendo passport e tabs
    Aleluia removendo os tabs. Aqui é bem mais fácil de editar! E esqueci de tirar o passport ontem. Enfim, dois coelhos em uma cajadada (nossa, nunca usei essa frase pra valer, primeira vez. rsss)

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 'use strict';
 
 var express = require('express')
-	, app = express()
-	, http = require('http').Server(app)
-	, path = require('path')
+  , app = express()
+  , http = require('http').Server(app)
+  , path = require('path')
 
-	, Grant = require('grant').express()
+  , Grant = require('grant').express()
 
-	, passport = require('passport')
-	, FacebookStrategy = require('passport-facebook').Strategy
-
-	, routes = require('./server/routes')
-	, authentication = require('./server/authentication')
-	, chat = require('./server/chat')
-	// configuração, vai ficar aqui por enquanto... depois preocupo com organizar o código node (aprender isso, na real rssss)
-	, localHost = '192.168.25.24'
-	, port = '3000'
-	;
+  , routes = require('./server/routes')
+  , authentication = require('./server/authentication')
+  , chat = require('./server/chat')
+  
+  , localHost = '192.168.25.24'
+  , port = '3000'
+  ;
 
 var grant = new Grant(require('./server/grant_config.json'));
 app.use(grant);
@@ -31,5 +28,5 @@ app.get('/', routes.root);
 chat.start(http);
 
 http.listen(port, function() {
-	console.log('Começou o jogo, em '+ localHost + ':' + port);
+  console.log('Começou o jogo, em '+ localHost + ':' + port);
 });
